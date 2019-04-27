5855c71e08d56920cbe8d1f5b8005e546f16fbb2
sheng zeng <bahtmannn@gmail.com>
2019-04-28 03:26:06 +1000

    show current location on map

--- frontend/components/LeafletMaps.vue
+++ frontend/components/LeafletMaps.vue
@@ -1,7 +1,8 @@
  <template>
-  <l-map style="height: 100%; width: 100%" :zoom="zoom" :center="center" @click='addPoint'>
+  <l-map style="height: 100%; width: 100%" :zoom="zoom" :center="currentLocation" @click='addPoint'>
     <l-tile-layer :url="url"></l-tile-layer>
     <l-polygon :lat-lngs="polygon.latlngs" :color="polygon.color"></l-polygon>
+        <l-marker :lat-lng="currentLocation" ></l-marker>
   </l-map>
 </template>
  
@@ -9,15 +10,19 @@
 export default {
   data () {
     return {
+      currentLocation: {lat:0,lng:0},
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      zoom: 8,
-      center: [47.413220, -1.319482],
-      markerLatLng: [47.313220, -1.319482],
+      zoom: 17,
+      center: [0, 0],
+      markerLatLng: [0, 0],
       polygon: {
         latlngs: [],
         color: 'green'
       },
     };
+  },
+  mounted: function(){
+      this.geolocation();
   },
   methods: {
 		addPoint(event) {
@@ -26,7 +31,16 @@ export default {
       }
       this.polygon.latlngs.push(event.latlng);
       console.log(this.polygon.latlngs)
-		}
+        },
+        geolocation(){
+                  navigator.geolocation.getCurrentPosition((position) => {
+        this.currentLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+      });
+        }
 	}
 };
 </script>
