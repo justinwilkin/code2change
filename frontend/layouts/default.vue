2610676b52b8653f603bf9ccfd86134a4a6ccd25
Sebastian Guillema <sguillema@hotmail.com>
2019-04-27 15:07:16 +1000

    Added some basic files!

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <nuxt />
+</template>
+
+<script>
+export default {
+  mounted() {
+    if (process.browser) {
+      screen.orientation.lock('portrait')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/styles/global.scss';
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="page-index">
+    <section class="section-container">
+      my name is jeff
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/styles/variables';
+</style>
