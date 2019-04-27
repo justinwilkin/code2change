a8781d19b8e711176435a7288adb3f41a5c8cc12
Sebastian Guillema <sguillema@hotmail.com>
2019-04-27 14:47:59 +1000

    Initial FE commit using Nuxt CLI

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,89 @@
+const pkg = require('./package')
+require('dotenv').config()
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.7.0/css/all.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: `https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons`
+      }
+    ]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
+  css: [{ src: 'normalize.css' }],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+  ],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
+  ],
+  /*
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.devtool = '#source-map'
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+
+  /*
+   ** Environment variables, with loading using Dotenv
+   */
+  env: {
+    // See https://github.com/motdotla/dotenv
+    // baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+  }
+}
