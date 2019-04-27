cc36a014435965395ebbebe5396aac6ea9df6b85
Aaron Gardoni <aarongardoni@gmail.com>
2019-04-27 15:35:16 +1000

    Flask backend initial commit

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/health')
+def health():
+    res = {'status': 'UP'}
+    return jsonify(res)
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
