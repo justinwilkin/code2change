15ef3f9a2b8216c26b2e83a193d8c8291fbfb300
Corey Stidston <corey.stidston@student.uts.edu.au>
2019-04-28 02:22:35 +1000

    Add polygon business

--- frontend/components/LeafletMaps.vue
+++ frontend/components/LeafletMaps.vue
@@ -1,32 +1,36 @@
  <template>
-       <l-map style="height: 100%; width: 100%" :zoom="zoom" :center="center">
+  <l-map style="height: 100%; width: 100%" :zoom="zoom" :center="center" @click='addPoint'>
     <l-tile-layer :url="url"></l-tile-layer>
-    <l-circle
-      :lat-lng="circle.center"
-      :radius="circle.radius"
-      :color="circle.color"
-    />
+    <l-polygon :lat-lngs="polygon.latlngs" :color="polygon.color"></l-polygon>
   </l-map>
- </template>
+</template>
  
  <script>
- export default {
-   data () {
+export default {
+  data () {
     return {
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       zoom: 8,
-      center: [47.313220, -1.319482],
-      circle: {
-        center: [47.413220, -1.0482],
-        radius: 4500,
-        color: 'red'
-      }
+      center: [47.413220, -1.319482],
+      markerLatLng: [47.313220, -1.319482],
+      polygon: {
+        latlngs: [],
+        color: 'green'
+      },
     };
-  }
- }
- </script>
+  },
+  methods: {
+		addPoint(event) {
+      if(this.polygon.latlngs.length == 4){
+      	this.polygon.latlngs = []
+      }
+      this.polygon.latlngs.push(event.latlng);
+      console.log(this.polygon.latlngs)
+		}
+	}
+};
+</script>
  
  <style>
- 
- </style>
+</style>
  
