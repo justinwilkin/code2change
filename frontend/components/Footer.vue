2fe459111d8333b0301a81d9fa0ce4e1f9eaa0d8
Justin Wilkin <justin.wilkin@digizoo.com.au>
2019-04-27 17:18:32 +1000

    Login Page

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <h2>This is the footer component</h2>
+    </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style>
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="page-index">
     <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   layout: 'default',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
