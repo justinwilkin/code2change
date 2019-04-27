3dd532ed26d65dd95a0859ff68501d8053fe2dad
Aaron Gardoni <aarongardoni@gmail.com>
2019-04-27 15:39:21 +1000

    Formatting

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/health')
 def health():
