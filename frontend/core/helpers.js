2135c28eae0f603e580a4b688c1cb5115edfcdb6
Aaron Gardoni <aarongardoni@gmail.com>
2019-04-28 10:20:32 +1000

    Added collision detection function for geo fence

--- frontend/core/helpers.js
+++ frontend/core/helpers.js
@@ -24,3 +24,10 @@ export const getExpiryOptions = shouldRemember => {
 export const storeModule = (moduleName, method) => {
   return `${moduleName}/${method}`
 }
+
+export const collides = (geo_fence, latlng) => {
+  return (latlng.lat < geo_fence[0].lat && latlng.lat < geo_fence[1].lat
+    && latlng.lat > geo_fence[2].lat && latlng.lat > geo_fence[3].lat
+    && latlng.lng < geo_fence[1].lng && latlng.lng < geo_fence[2].lng
+    && latlng.lng > geo_fence[0].lng && latlng.lng > geo_fence[3].lng)
+}
